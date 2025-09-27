1b0a8af434e7deef6bb6ab189a1502c476b93b79
Aryamik <aryamik.b7@gmail.com>
2025-09-27 12:52:58 +0530

    added linked list operations

--- LinkedList.py
+++ LinkedList.py
@@ -42,4 +42,55 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+        
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self,index,value):
+        if index<0 or index>=self.length:
+            return False
+        if index==0:
+            return self.prepend(value)
+        if index==self.length:
+            return self.append(value)
+        newNode=Node(value)
+        temp=self.get(index-1)
+        newNode.next=temp.next
+        temp.next=newNode
+        self.length+=1
+        return True
+        
